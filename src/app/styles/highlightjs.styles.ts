acc9967a5813c0d2910eaaeb41ea117c279241aa
KristofferRoenLie <kristoffer.roenlie@eye-share.no>
2023-01-27 14:48:24 +0100

    Added missing root css spesifier to hljs theme.

--- src/app/styles/highlightjs.styles.ts
+++ src/app/styles/highlightjs.styles.ts
@@ -1,186 +1,186 @@
 import { css } from 'lit';
 
 export const highlightjsStyles = css`
-[color-scheme="dark"] .hljs {
+:root:root[color-scheme="dark"] .hljs {
 	color: #adbac7;
 	background: #22272e
 }
 
-[color-scheme="dark"] .hljs-doctag,
-[color-scheme="dark"] .hljs-keyword,
-[color-scheme="dark"] .hljs-meta .hljs-keyword,
-[color-scheme="dark"] .hljs-template-tag,
-[color-scheme="dark"] .hljs-template-variable,
-[color-scheme="dark"] .hljs-type,
-[color-scheme="dark"] .hljs-variable.language_ {
+:root[color-scheme="dark"] .hljs-doctag,
+:root[color-scheme="dark"] .hljs-keyword,
+:root[color-scheme="dark"] .hljs-meta .hljs-keyword,
+:root[color-scheme="dark"] .hljs-template-tag,
+:root[color-scheme="dark"] .hljs-template-variable,
+:root[color-scheme="dark"] .hljs-type,
+:root[color-scheme="dark"] .hljs-variable.language_ {
 	color: #f47067
 }
 
-[color-scheme="dark"] .hljs-title,
-[color-scheme="dark"] .hljs-title.class_,
-[color-scheme="dark"] .hljs-title.class_.inherited__,
-[color-scheme="dark"] .hljs-title.function_ {
+:root[color-scheme="dark"] .hljs-title,
+:root[color-scheme="dark"] .hljs-title.class_,
+:root[color-scheme="dark"] .hljs-title.class_.inherited__,
+:root[color-scheme="dark"] .hljs-title.function_ {
 	color: #dcbdfb
 }
 
-[color-scheme="dark"] .hljs-attr,
-[color-scheme="dark"] .hljs-attribute,
-[color-scheme="dark"] .hljs-literal,
-[color-scheme="dark"] .hljs-meta,
-[color-scheme="dark"] .hljs-number,
-[color-scheme="dark"] .hljs-operator,
-[color-scheme="dark"] .hljs-selector-attr,
-[color-scheme="dark"] .hljs-selector-class,
-[color-scheme="dark"] .hljs-selector-id,
-[color-scheme="dark"] .hljs-variable {
+:root[color-scheme="dark"] .hljs-attr,
+:root[color-scheme="dark"] .hljs-attribute,
+:root[color-scheme="dark"] .hljs-literal,
+:root[color-scheme="dark"] .hljs-meta,
+:root[color-scheme="dark"] .hljs-number,
+:root[color-scheme="dark"] .hljs-operator,
+:root[color-scheme="dark"] .hljs-selector-attr,
+:root[color-scheme="dark"] .hljs-selector-class,
+:root[color-scheme="dark"] .hljs-selector-id,
+:root[color-scheme="dark"] .hljs-variable {
 	color: #6cb6ff
 }
 
-[color-scheme="dark"] .hljs-meta .hljs-string,
-[color-scheme="dark"] .hljs-regexp,
-[color-scheme="dark"] .hljs-string {
+:root[color-scheme="dark"] .hljs-meta .hljs-string,
+:root[color-scheme="dark"] .hljs-regexp,
+:root[color-scheme="dark"] .hljs-string {
 	color: #96d0ff
 }
 
-[color-scheme="dark"] .hljs-built_in,
-[color-scheme="dark"] .hljs-symbol {
+:root[color-scheme="dark"] .hljs-built_in,
+:root[color-scheme="dark"] .hljs-symbol {
 	color: #f69d50
 }
 
-[color-scheme="dark"] .hljs-code,
-[color-scheme="dark"] .hljs-comment,
-[color-scheme="dark"] .hljs-formula {
+:root[color-scheme="dark"] .hljs-code,
+:root[color-scheme="dark"] .hljs-comment,
+:root[color-scheme="dark"] .hljs-formula {
 	color: #768390
 }
 
-[color-scheme="dark"] .hljs-name,
-[color-scheme="dark"] .hljs-quote,
-[color-scheme="dark"] .hljs-selector-pseudo,
-[color-scheme="dark"] .hljs-selector-tag {
+:root[color-scheme="dark"] .hljs-name,
+:root[color-scheme="dark"] .hljs-quote,
+:root[color-scheme="dark"] .hljs-selector-pseudo,
+:root[color-scheme="dark"] .hljs-selector-tag {
 	color: #8ddb8c
 }
 
-[color-scheme="dark"] .hljs-subst {
+:root[color-scheme="dark"] .hljs-subst {
 	color: #adbac7
 }
 
-[color-scheme="dark"] .hljs-section {
+:root[color-scheme="dark"] .hljs-section {
 	color: #316dca;
 	font-weight: 700
 }
 
-[color-scheme="dark"] .hljs-bullet {
+:root[color-scheme="dark"] .hljs-bullet {
 	color: #eac55f
 }
 
-[color-scheme="dark"] .hljs-emphasis {
+:root[color-scheme="dark"] .hljs-emphasis {
 	color: #adbac7;
 	font-style: italic
 }
 
-[color-scheme="dark"] .hljs-strong {
+:root[color-scheme="dark"] .hljs-strong {
 	color: #adbac7;
 	font-weight: 700
 }
 
-[color-scheme="dark"] .hljs-addition {
+:root[color-scheme="dark"] .hljs-addition {
 	color: #b4f1b4;
 	background-color: #1b4721
 }
 
-[color-scheme="dark"] .hljs-deletion {
+:root[color-scheme="dark"] .hljs-deletion {
 	color: #ffd8d3;
 	background-color: #78191b
 }
 
-[color-scheme="light"] .hljs {
+:root[color-scheme="light"] .hljs {
 	color: #24292e;
 	background: #fff
 }
 
-[color-scheme="light"] .hljs-doctag,
-[color-scheme="light"] .hljs-keyword,
-[color-scheme="light"] .hljs-meta .hljs-keyword,
-[color-scheme="light"] .hljs-template-tag,
-[color-scheme="light"] .hljs-template-variable,
-[color-scheme="light"] .hljs-type,
-[color-scheme="light"] .hljs-variable.language_ {
+:root[color-scheme="light"] .hljs-doctag,
+:root[color-scheme="light"] .hljs-keyword,
+:root[color-scheme="light"] .hljs-meta .hljs-keyword,
+:root[color-scheme="light"] .hljs-template-tag,
+:root[color-scheme="light"] .hljs-template-variable,
+:root[color-scheme="light"] .hljs-type,
+:root[color-scheme="light"] .hljs-variable.language_ {
 	color: #d73a49
 }
 
-[color-scheme="light"] .hljs-title,
-[color-scheme="light"] .hljs-title.class_,
-[color-scheme="light"] .hljs-title.class_.inherited__,
-[color-scheme="light"] .hljs-title.function_ {
+:root[color-scheme="light"] .hljs-title,
+:root[color-scheme="light"] .hljs-title.class_,
+:root[color-scheme="light"] .hljs-title.class_.inherited__,
+:root[color-scheme="light"] .hljs-title.function_ {
 	color: #6f42c1
 }
 
-[color-scheme="light"] .hljs-attr,
-[color-scheme="light"] .hljs-attribute,
-[color-scheme="light"] .hljs-literal,
-[color-scheme="light"] .hljs-meta,
-[color-scheme="light"] .hljs-number,
-[color-scheme="light"] .hljs-operator,
-[color-scheme="light"] .hljs-selector-attr,
-[color-scheme="light"] .hljs-selector-class,
-[color-scheme="light"] .hljs-selector-id,
-[color-scheme="light"] .hljs-variable {
+:root[color-scheme="light"] .hljs-attr,
+:root[color-scheme="light"] .hljs-attribute,
+:root[color-scheme="light"] .hljs-literal,
+:root[color-scheme="light"] .hljs-meta,
+:root[color-scheme="light"] .hljs-number,
+:root[color-scheme="light"] .hljs-operator,
+:root[color-scheme="light"] .hljs-selector-attr,
+:root[color-scheme="light"] .hljs-selector-class,
+:root[color-scheme="light"] .hljs-selector-id,
+:root[color-scheme="light"] .hljs-variable {
 	color: #005cc5
 }
 
-[color-scheme="light"] .hljs-meta .hljs-string,
-[color-scheme="light"] .hljs-regexp,
-[color-scheme="light"] .hljs-string {
+:root[color-scheme="light"] .hljs-meta .hljs-string,
+:root[color-scheme="light"] .hljs-regexp,
+:root[color-scheme="light"] .hljs-string {
 	color: #032f62
 }
 
-[color-scheme="light"] .hljs-built_in,
-[color-scheme="light"] .hljs-symbol {
+:root[color-scheme="light"] .hljs-built_in,
+:root[color-scheme="light"] .hljs-symbol {
 	color: #e36209
 }
 
-[color-scheme="light"] .hljs-code,
-[color-scheme="light"] .hljs-comment,
-[color-scheme="light"] .hljs-formula {
+:root[color-scheme="light"] .hljs-code,
+:root[color-scheme="light"] .hljs-comment,
+:root[color-scheme="light"] .hljs-formula {
 	color: #6a737d
 }
 
-[color-scheme="light"] .hljs-name,
-[color-scheme="light"] .hljs-quote,
-[color-scheme="light"] .hljs-selector-pseudo,
-[color-scheme="light"] .hljs-selector-tag {
+:root[color-scheme="light"] .hljs-name,
+:root[color-scheme="light"] .hljs-quote,
+:root[color-scheme="light"] .hljs-selector-pseudo,
+:root[color-scheme="light"] .hljs-selector-tag {
 	color: #22863a
 }
 
-[color-scheme="light"] .hljs-subst {
+:root[color-scheme="light"] .hljs-subst {
 	color: #24292e
 }
 
-[color-scheme="light"] .hljs-section {
+:root[color-scheme="light"] .hljs-section {
 	color: #005cc5;
 	font-weight: 700
 }
 
-[color-scheme="light"] .hljs-bullet {
+:root[color-scheme="light"] .hljs-bullet {
 	color: #735c0f
 }
 
-[color-scheme="light"] .hljs-emphasis {
+:root[color-scheme="light"] .hljs-emphasis {
 	color: #24292e;
 	font-style: italic
 }
 
-[color-scheme="light"] .hljs-strong {
+:root[color-scheme="light"] .hljs-strong {
 	color: #24292e;
 	font-weight: 700
 }
 
-[color-scheme="light"] .hljs-addition {
+:root[color-scheme="light"] .hljs-addition {
 	color: #22863a;
 	background-color: #f0fff4
 }
 
-[color-scheme="light"] .hljs-deletion {
+:root[color-scheme="light"] .hljs-deletion {
 	color: #b31d28;
 	background-color: #ffeef0
 }
