b13ef7827af5e8bf77fa834c5eb49bf305be8d13
KristofferRoenLie <KristofferRoenLie@gmail.com>
2023-11-30 00:52:37 +0100

    Added getter to page header to make lit happy.

--- src/app/components/page-header.ts
+++ src/app/components/page-header.ts
@@ -12,11 +12,16 @@ export class MiDocHeaderCmp extends LitElement {
 
 	@property({ type: Boolean }) public editor?: boolean;
 	@property({ type: Object }) public declaration: Declarations;
+
 	@property({ attribute: 'component-name' }) public set componentName(v: string) {
 		const old = this._name;
 		this._slug = v.slice(v.indexOf('-') + 1);
 		this._name = toWords(toPascalCase(this._slug));
 		this.requestUpdate('name', old);
+	}
+
+	public get componentName() {
+		return this._name;
 	}
 
 	protected _name = '';
